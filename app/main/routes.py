e5ab82797ff59fe3b37c2bcc6b9b4dd33b52a0ae
Charlie Griefer <charlie@griefer.com>
2023-12-21 16:39:47 -0700

    For #5 - added before_request method to return dogs object

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,34 +6,33 @@ from flask import current_app, render_template
 from app.extensions import cache
 from app.main import main_blueprint
 
+@main_blueprint.before_request
+def load_available_dogs():
+    if not cache.has("available_dogs"):
+        print("GETTING DOGS!!!")
+        PETSTABLISHED_BASE_URL = current_app.config['PETSTABLISHED_BASE_URL']
+        PETSTABLISHED_PUBLIC_KEY = current_app.config['PETSTABLISHED_PUBLIC_KEY']
+
+        pet_url = f"{PETSTABLISHED_BASE_URL}?public_key={PETSTABLISHED_PUBLIC_KEY}"
+        available_dogs = f"{pet_url}&search[status]=Available&sort[order]=asc&sort[column]=name&pagination[limit]=100"
+
+        r = requests.get(available_dogs)
+        available_dogs = r.json()
+        cache.set("available_dogs", available_dogs, timeout=3600)
+
 @main_blueprint.route("/")
 @main_blueprint.route("/index")
 def index():
-    PETSTABLISHED_BASE_URL = current_app.config['PETSTABLISHED_BASE_URL']
-    PETSTABLISHED_PUBLIC_KEY = current_app.config['PETSTABLISHED_PUBLIC_KEY']
-
-    pet_url = f"{PETSTABLISHED_BASE_URL}?public_key={PETSTABLISHED_PUBLIC_KEY}"
-
-    available_dogs_url = f"{pet_url}&search[status]=Available&sort[order]=asc&sort[column]=name&pagination[limit]=100"
-
-    if not cache.has("dogs"):
-        r = requests.get(available_dogs_url)
-        dogs = r.json()
-        cache.set("dogs", dogs, timeout=3600)
-    else:
-        dogs = cache.get("dogs")
-
-    return render_template("index.html", title="Home", dogs=dogs.get("collection"))
+    available_dogs = cache.get("available_dogs")
+    return render_template("index.html", title="Home", dogs=available_dogs.get("collection"))
 
 
-@main_blueprint.route("/dog/<string:dog_name>")
-def dog(dog_name: str):
-    PETSTABLISHED_BASE_URL = current_app.config['PETSTABLISHED_BASE_URL']
-    PETSTABLISHED_PUBLIC_KEY = current_app.config['PETSTABLISHED_PUBLIC_KEY']
+@main_blueprint.route("/detail/<int:dog_id>")
+def dog_detail(dog_id: int):
+    available_dogs = cache.get("available_dogs")
 
-    dog_url = f"{PETSTABLISHED_BASE_URL}?public_key={PETSTABLISHED_PUBLIC_KEY}&search[name]={dog_name}"
+    dog = next((item for item in available_dogs.get("collection") if item["id"] == dog_id), None)
+    if not dog:
+        return render_template("404.html")
 
-    r = requests.get(dog_url)
-    dog = r.json()
-    # TODO: handle no dog returned
-    return render_template("detail.html", dog=dog.get("collection")[0])
+    return render_template("detail.html", dog=dog)
