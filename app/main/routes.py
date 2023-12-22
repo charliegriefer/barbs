9636e5c8ae84862110f322137aef995d95b32536
Charlie Griefer <charlie@griefer.com>
2023-12-22 08:59:04 -0700

    For #10 - refactor some code

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,5 @@
 import json
+from typing import Dict, List
 
 import requests
 from flask import current_app, render_template, request
@@ -21,6 +22,7 @@ def load_available_dogs():
         available_dogs = r.json()
         cache.set("available_dogs", available_dogs, timeout=3600)
 
+
 @main_blueprint.route("/", methods=["GET", "POST"])
 @main_blueprint.route("/index", methods=["GET", "POST"])
 def index():
@@ -28,36 +30,23 @@ def index():
     form = SearchForm()
 
     # create the list of breeds to populate the dropdown
-    breeds = set()
-    [breeds.add(breed.get("primary_breed")) for breed in available_dogs if breed.get("primary_breed") != ""]
-    [breeds.add(breed.get("secondary_breed")) for breed in available_dogs if breed.get("secondary_breed") != ""]
-
-    form_breeds = list(breeds)
-    form_breeds.sort()
-
-    form.breed.choices = [(breed, breed) for breed in form_breeds]
+    form.breed.choices = [(breed, breed) for breed in get_dog_breeds(available_dogs)]
     form.breed.choices.insert(0, ("", "Any"))
 
-    if request.method == "POST":
-        if request.form["gender"] != "":
-            available_dogs = [dog for dog in available_dogs if dog["sex"] == request.form["gender"]]
-        if request.form["age"] != "":
-            available_dogs = [dog for dog in available_dogs if dog["age"] == request.form["age"]]
-        if request.form["breed"] != "":
-            available_dogs = [dog for dog in available_dogs
-                if dog["primary_breed"] == request.form["breed"] or dog["secondary_breed"] == request.form["breed"]]
-        if request.form["shedding"] != "":
-            available_dogs = [dog for dog in available_dogs if dog["shedding"] == request.form["shedding"]]
-        if request.form["size"] != "":
-            available_dogs = [dog for dog in available_dogs if dog["size"] == request.form["size"]]
-        if request.form.get("is_ok_with_other_dogs"):
-            available_dogs = [dog for dog in available_dogs if dog["is_ok_with_other_dogs"] == "Yes"]
-        if request.form.get("is_ok_with_other_cats"):
-            available_dogs = [dog for dog in available_dogs if dog["is_ok_with_other_cats"] == "Yes"]
-        if request.form.get("is_ok_with_other_kids"):
-            available_dogs = [dog for dog in available_dogs if dog["is_ok_with_other_kids"] == "Yes"]
+    # do filtering
+    for arg in request.args:
+        if request.args.get(arg):
+            if arg in ["sex", "age", "size", "shedding"]:
+                available_dogs = [dog for dog in available_dogs if dog[arg] == request.args.get(arg)]
+                form[arg].process_data(request.args.get(arg))
+            if arg in ["is_ok_with_other_dogs", "is_ok_with_other_cats", "is_ok_with_other_kids"]:
+                available_dogs = [dog for dog in available_dogs if dog[arg] == "Yes"]
+                form[arg].process_data(request.args.get(arg))
 
-    return render_template("index.html", title="Home", form=form, dogs=available_dogs)
+    return render_template("index.html",
+                           title="Adopt | Puerto Peñasco | Barb's Dog Rescue",
+                           form=form,
+                           dogs=available_dogs)
 
 
 @main_blueprint.route("/detail/<int:dog_id>")
@@ -69,3 +58,16 @@ def dog_detail(dog_id: int):
         return render_template("404.html")
 
     return render_template("detail.html", dog=dog)
+
+
+def get_dog_breeds(available_dogs: List[Dict[str, str]]) -> List[str]:
+    """
+    Given a list of available dogs, return a list of distinct breeds
+    """
+    breeds = set()
+    [breeds.add(breed.get("primary_breed")) for breed in available_dogs if breed.get("primary_breed") != ""]
+    [breeds.add(breed.get("secondary_breed")) for breed in available_dogs if breed.get("secondary_breed") != ""]
+
+    form_breeds = list(breeds)
+    form_breeds.sort()
+    return form_breeds
