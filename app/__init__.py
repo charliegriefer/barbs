57740a0cdaf4e30ed6903d57903c48b52459d2eb
Charlie Griefer <charlie@griefer.com>
2023-12-29 08:13:40 -0700

    For #7 - fix up the 404

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ import os
 
 import flask
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template
 from flask_wtf.csrf import CSRFProtect
 
 from app.extensions import cache
@@ -12,6 +12,10 @@ load_dotenv(dotenv_path=dotenv_path, verbose=True)
 
 csrf = CSRFProtect()
 
+
+def page_not_found(e):
+    return render_template("404.html"), 404
+
 def create_app():
     application = Flask(__name__)
 
@@ -19,6 +23,8 @@ def create_app():
 
     cache.init_app(application)
     csrf.init_app(application)
+
+    application.register_error_handler(404, page_not_found)
 
     # register blueprints
     from app.main import main_blueprint
